7e5d4b3f4db67d721fa19d3c70924b4f9849d406
svonk <example@mail.ru>
2021-11-02 16:10:51 +0300

    Some fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
 window.addEventListener('load', function () {
     const form = document.querySelector('.wrapper');
-    const btn = document.querySelector('.button');
 
 
-    ['focusout', 'focusin'].forEach((event) => {
-
+    ['focusout', 'focusin', 'click'].forEach((event) => {
         form.addEventListener(event, (e) => {
-            if (e.target.classList.contains('check')) {
-                switch (event) {
-                    case 'focusout':
+            switch (event) {
+                case 'focusout':
+                    if (e.target.classList.contains('check')) {
                         if (e.target.value.length <= 0) {
                             e.target.classList.add('err')
-                            btn.disabled = true
                         }
+                    }
+                    break;
+                case 'focusin':
+                    e.target.classList.remove('err')
+                    break;
+                case 'click':
+                    if (e.target.classList.contains('button')) {
+                        e.preventDefault()
 
-                        break;
-                    case 'focusin':
-                        e.target.classList.remove('err')
-                        break;
-                }
+
+                        console.log('btn')
+                    }
+                    break;
 
             }
-        });
-    })
 
+        });
+
+    })
 
 })
