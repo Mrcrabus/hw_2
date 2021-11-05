7ae0d3f819a3f23757ff352f93e662cbcde3085c
svonk <example@mail.ru>
2021-11-05 10:48:51 +0300

    Have to some fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     const form = document.querySelector('.wrapper');
 
 
-    ['focusout', 'focusin', 'click'].forEach((event) => {
+    ['focusout', 'focusin', 'submit'].forEach((event) => {
         form.addEventListener(event, (e) => {
             switch (event) {
                 case 'focusout':
@@ -15,15 +15,20 @@ window.addEventListener('load', function () {
                 case 'focusin':
                     e.target.classList.remove('err')
                     break;
-                case 'click':
-                    if (e.target.classList.contains('button')) {
-                        e.preventDefault()
+                case 'submit':
 
+                    if (e.target.classList.contains('wrapper')) {
 
-                        console.log('btn')
+                        for (let i = 0; i < (e.target.length - 1); i++) {
+                            if (e.target[i].value.length <= 0) {
+                                e.target[i].classList.add('err')
+                                e.preventDefault()
+
+                            }
+
+                        }
                     }
                     break;
-
             }
 
         });
